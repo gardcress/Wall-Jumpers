937ffc7329365f17c69a617f6cfcc8ba642246d7
ShadArdalani <shad.ardalani@gmail.com>
2025-05-02 18:41:06 +0200

    Update WallIncreaseScore.cs

--- Assets/Scripts/WallIncreaseScore.cs
+++ Assets/Scripts/WallIncreaseScore.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class WallIncreaseScore : MonoBehaviour
 {
+    private bool hasBeenHit = false;  // <- Track if this wall was already hit
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        if (collision.gameObject.CompareTag("Player"))
+        if (!hasBeenHit && collision.gameObject.CompareTag("Player"))
         {
-
             Score.instance.UpdateScore();
+            hasBeenHit = true;  // <- Mark this wall as already hit
         }
     }
 }
