4007f86348a98a20e93ad7e63bd7c7193dd78742
kevin jager <kevin.jager@live.com>
2025-05-06 13:02:54 +0200

    fixed death

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
-    public Transform groundReference; // Assign the ground object here
+    //public Transform groundReference; // Assign the ground object here
     public string menuSceneName = "MenuScene";
 
     void Start()
@@ -14,9 +14,9 @@ public class GameOverScript : MonoBehaviour
 
     void Update()
     {
-        if (groundReference == null) return;
+        //if (groundReference == null) return;
 
-        if (transform.position.y < (groundReference.position.y - 2.0f))
+        if (transform.position.y < -10)
         {
             SceneManager.LoadScene(menuSceneName);
         }
